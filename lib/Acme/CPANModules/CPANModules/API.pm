73981cf19586798867c8828bd8b56fa71c1bb890
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-06 09:31:14 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/CPANModules/API.pm
@@ -0,0 +1,19 @@
+package Acme::CPANModules::CPANModules;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "Acme::CPANModules modules relating to API",
+    entries => [
+        {module=>'Acme::CPANModules::API::Dead::Currency'},
+        {module=>'Acme::CPANModules::API::Domain::Registrar'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
